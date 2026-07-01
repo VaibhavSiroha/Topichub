9588885da906361356a462ed701f2e597efb7046
Nocturnal <vaibhavsiroha2000@gmail.com>
2026-07-01 22:20:27 +0530

    Production ready

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Vercel build step: install deps and collect static files into STATIC_ROOT.
-pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
+# Vercel's build image is an "externally managed" (PEP 668) Python env, so pip
+# needs --break-system-packages to install into it.
+set -e
+
+python3 -m pip install --break-system-packages -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
